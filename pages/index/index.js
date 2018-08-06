cef7908752a50bba5cb6cf553fa0807e5dfc9e87
taojin <taojin@xiaomi.com>
2018-08-06 19:42:19 +0800

    first commit

---
+++ pages/index/index.js
@@ -0,0 +1,168 @@
+const app = getApp();
+Page({
+  data: {
+    movies: [],
+    tab_num: '1',
+    userInfo: {},
+    chatMsgs: [],
+    txt: '',
+    scrollTop: 0,
+    userInfoBtnHidden: false
+  },
+  onLoad: function() {
+    let $this = this;
+    wx.setNavigationBarTitle({
+      title: '月牙爱看-热映电影'
+    })
+    // 电影
+    wx.request({
+      url: 'https://small.tjzmy.cn/v2/movie/in_theaters',
+      header: {
+        "Content-Type": "json"
+      },
+      success: function(res) {
+        $this.setData({
+          movies: res.data.subjects
+        })
+      }
+    })
+  },
+  startSocket: function() {
+    var $this = this;
+    wx.connectSocket({
+      url: 'wss://small.tjzmy.cn/socket/?img=' + this.data.userInfo.avatarUrl,
+      header: {
+        'content-type': 'application/json'
+      }
+    })
+    wx.onSocketOpen(function (res) {
+      console.log('WebSocket连接已打开！');
+      var msg = '大家好';
+      wx.sendSocketMessage({
+        data: msg,
+        fail: function (res) {
+          console.log('发送失败1', res)
+        },
+        success: function(res) {
+          $this.render(msg, $this.data.userInfo.avatarUrl, true);
+        }
+      })
+    })
+    wx.onSocketError(function (res) {
+      console.log('WebSocket连接打开失败，请检查！');
+    })
+    wx.onSocketMessage(function (res) {
+      console.log('收到服务器内容：' + res.data);
+      var resdata = JSON.parse(res.data);
+      $this.render(resdata.message, resdata.img, false);
+    })
+    wx.onSocketClose(function(res) {
+      console.log('Socket链接关闭');
+      setTimeout(function() {
+        $this.reconnect();
+      }, 1000)
+    })
+  },
+  reconnect: function(msg) {
+    var $this = this;
+    wx.connectSocket({
+      url: 'wss://small.tjzmy.cn/socket/?img=' + $this.data.userInfo.avatarUrl,
+      header: {
+        'content-type': 'application/json'
+      }
+    })
+    wx.onSocketOpen(function (res) {
+      console.log('WebSocket连接已打开！again');
+      if (msg) {
+        wx.sendSocketMessage({
+          data: msg,
+          fail: function(res) {
+            console.log('重新链接并发送失败', res);
+          },
+          success: function(res) {
+            $this.render(msg, $this.data.userInfo.avatarUrl, true);
+          }
+        })
+      }
+    })
+  },
+  detail: function(event) {
+    let movie = event.currentTarget.dataset.movie;
+    wx.navigateTo({
+      url: '../movieDetail/movieDetail?id=' + movie.id
+    })
+  },
+  getUserInfoFun: function () {
+    var S = this;
+    wx.getUserInfo({
+      success: function (res) {
+        console.log("userInfo:" + res);
+        app.globalData.userInfo = res.userInfo;
+        S.setData({
+          userInfo: res.userInfo
+        })
+        S.startSocket();
+      },
+      fail: res => {
+        var userInfo = {
+          avatarUrl: 'http://o71pfzm86.bkt.clouddn.com/u=1587665103,1340804954&fm=21&gp=0.jpg'
+        }
+        app.globalData.userInfo = userInfo
+        this.setData({
+          userInfo: userInfo
+        })
+        this.startSocket();
+      }
+    })
+  },
+  tabSwitch: function(event) {
+    let $this = this;
+    $this.setData({
+      tab_num: event.currentTarget.dataset.num
+    })
+    // 获取用户头像，如果拒绝，用默认头像
+    if (event.currentTarget.dataset.num == '2' && !$this.data.userInfoBtnHidden) {
+      $this.setData({
+        userInfoBtnHidden: true
+      });
+    }
+  },
+  bindFormSubmit: function (e) {
+    var $this = this;
+    console.log(e.detail.value.textarea)
+    var msg = e.detail.value.textarea;
+    if (!msg) {
+      return;
+    }
+    wx.sendSocketMessage({
+      data: msg,
+      fail: function (res) {
+        console.log('发送失败2', res)
+        $this.reconnect(msg);
+      },
+      success: function(res) {
+        $this.render(msg, $this.data.userInfo.avatarUrl, true);
+      }
+    })
+    this.setData({
+      txt: ''
+    })
+  },
+  render: function(msg, img, isOwn) {
+    var $this = this;
+    var chat = {
+      img: img,
+      msg: msg,
+      isOwn: isOwn
+    }
+    var chatMsgs = $this.data.chatMsgs;
+    chatMsgs.push(chat);
+    $this.setData({
+      chatMsgs: chatMsgs
+    })
+    var length = chatMsgs.length;
+    $this.setData({
+      scrollTop: 100 * chatMsgs.length
+    })
+  }
+})
---
+++ pages/movieDetail/movieDetail.js
@@ -0,0 +1,122 @@
+// pages/movieDetail/movieDetail.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    show: false,
+    detail: {},
+    photos: [],
+    imagesList: []
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    let $this = this;
+    // 获取保存到相册的权限
+    wx.getSetting({
+      success: res => {
+        if (!res.authSetting['scope.writePhotosAlbum']) {
+          wx.authorize({
+            scope: 'scope.writePhotosAlbum',
+            success() {
+              console.log('授权成功')
+            }
+          })
+        }
+      }
+    })
+    wx.request({
+      url: 'https://small.tjzmy.cn/v2/movie/subject/' + options.id,
+      header: {
+        'content-type': 'json'
+      },
+      success: function(res) {
+        $this.setData({
+          detail: res.data,
+          show: true
+        })
+        wx.setNavigationBarTitle({
+          title: '月牙爱看-' + res.data.title
+        })
+      }
+    })
+    wx.request({
+      url: 'https://small.tjzmy.cn/v2/movie/subject/' + options.id + '/photos?apikey=0df993c66c0c636e29ecbb5344252a4a',
+      header: {
+        'content-type': 'json'
+      },
+      success: function (res) {
+        let arr = [];
+        for(let i=0;i<res.data.photos.length;i++) {
+          let photo = res.data.photos[i];
+          arr.push(photo.image);
+        }
+        $this.setData({
+          photos: res.data.photos,
+          imagesList: arr
+        })
+      }
+    })
+  },
+  preview: function(event) {
+    let $this = this;
+    let src = event.currentTarget.dataset.src;
+    wx.previewImage({
+      current: src,
+      urls: $this.data.imagesList,
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
