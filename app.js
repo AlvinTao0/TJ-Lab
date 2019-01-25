b497aa76cef3b98ba4d8d88996dc82e6caba0180
taojin <taojin@xiaomi.com>
2019-01-25 18:30:21 +0800

    feat 新增watch监听

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 App({
   onLaunch: function () {
     var S = this;
-    console.log(S)
     // 登录
     wx.login({
       success: res => {
@@ -26,6 +25,32 @@ App({
             }
           })
         }
+      }
+    })
+  },
+  /**
+    * 设置监听器
+    */
+  setWatcher(data, watch, currentThis) { // 接收index.js传过来的data对象和watch对象
+    Object.keys(watch).forEach(v => { // 将watch对象内的key遍历
+      this.observe(data, v, watch[v], currentThis); // 监听data内的v属性，传入watch内对应函数以调用
+    })
+  },
+
+  /**
+   * 监听属性 并执行监听函数
+   */
+  observe(obj, key, watchFun, currentThis) {
+    var val = obj[key]; // 给该属性设默认值
+    Object.defineProperty(obj, key, {
+      configurable: true,
+      enumerable: true,
+      set: function (value) {
+        val = value;
+        watchFun.call(currentThis, value, val); // 赋值(set)时，调用对应函数
+      },
+      get: function () {
+        return val;
       }
     })
   },
